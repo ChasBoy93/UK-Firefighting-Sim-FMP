e37465da8fa43bcde48a107de4b081738e54b34f
Charlie Wood <549750@chrome.escg.ac.uk>
2025-03-20 10:34:04 +0000

    truck controller start

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,82 +1,157 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private float horizontalInput, verticalInput;
-    private float currentSteerAngle, currentbreakForce;
-    private bool isBreaking;
+    [Header("Referances")]
+    public Rigidbody truckRB;
+    public Transform[] rayPoints;
+    public LayerMask driveable;
+    public Transform accelerationPoint;
 
-    // Settings
-    [SerializeField] private float motorForce, breakForce, maxSteerAngle;
+    [Header("Suspension Settings")]
+    public float springStiffness;
+    public float damperStiffness;
+    public float restLength;
+    public float springTravel;
+    public float wheelRadius;
 
-    // Wheel Colliders
-    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
-    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+    private int[] wheelsIsGrounded = new int[4];
+    private bool isGrounded = false;
 
-    // Wheels
-    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
-    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+    [Header("Input")]
+    private float moveInput = 0;
+    private float steerInput = 0;
 
-    private void FixedUpdate()
+    [Header("Car Settings")]
+    public float acceleration = 25f;
+    public float maxSpeed = 56f;
+    public float deceleration = 10f;
+
+    private Vector3 currentCarLocalVelocity = Vector3.zero;
+    private float carVelocityRatio = 0;
+
+    #region Unity Functions
+
+    void Start()
     {
-        GetInput();
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();
+        truckRB = GetComponent<Rigidbody>();
     }
 
-    private void GetInput()
+    void FixedUpdate()
     {
-        // Steering Input
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        // Acceleration Input
-        verticalInput = Input.GetAxis("Vertical");
-
-        // Breaking Input
-        isBreaking = Input.GetKey(KeyCode.Space);
+        Suspension(); 
+        GroundCheck();
+        CalculateCarVelocity();
+        Movment();
     }
 
-    private void HandleMotor()
+    void Update()
     {
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
-        currentbreakForce = isBreaking ? breakForce : 0f;
-        ApplyBreaking();
+        GetPlayerInput();
     }
 
-    private void ApplyBreaking()
+    #endregion
+
+    #region Movment
+
+    void Movment()
     {
-        frontRightWheelCollider.brakeTorque = currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearRightWheelCollider.brakeTorque = currentbreakForce;
+        if(isGrounded)
+        {
+            Acceleration();
+            Decelatation();
+        }
     }
 
-    private void HandleSteering()
+    void Acceleration()
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        truckRB.AddForceAtPosition(acceleration * moveInput * transform.forward, accelerationPoint.position, ForceMode.Acceleration);
     }
 
-    private void UpdateWheels()
+    void Decelatation()
     {
-        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        truckRB.AddForceAtPosition(deceleration * moveInput * -transform.forward, accelerationPoint.position, ForceMode.Acceleration);
     }
 
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    #endregion
+
+    #region Check Status Truck
+
+    void GroundCheck()
     {
-        Vector3 pos;
-        Quaternion rot;
-        wheelCollider.GetWorldPose(out pos, out rot);
-        wheelTransform.rotation = rot;
-        wheelTransform.position = pos;
+        int tempGroundedWheels = 0;
+
+        for (int i = 0; i < wheelsIsGrounded.Length; i++)
+        {
+            tempGroundedWheels += wheelsIsGrounded[i];
+        }
+
+        if(tempGroundedWheels > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
+
+    private void CalculateCarVelocity()
+    {
+        currentCarLocalVelocity = transform.InverseTransformDirection(truckRB.linearVelocity);
+        carVelocityRatio = currentCarLocalVelocity.z / maxSpeed;
+    }
+
+    #endregion
+
+    #region Input Handeling
+
+    void GetPlayerInput()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Hoizontal");
+    }
+
+    #endregion
+
+    #region Suspension Functions
+
+    void Suspension()
+    {
+        for(int i = 0; i < rayPoints.Length; i++)
+        {
+            RaycastHit hit;
+            float maxLength = restLength + springTravel;
+
+            if (Physics.Raycast(rayPoints[i].position, -rayPoints[i].up, out hit, maxLength + wheelRadius, driveable))
+            {
+                wheelsIsGrounded[i] = 1;
+
+                float currentSpringLength = hit.distance - wheelRadius;
+                float springCompression = restLength - currentSpringLength / springTravel;
+
+                float springVelocity = Vector3.Dot(truckRB.GetPointVelocity(rayPoints[i].position), rayPoints[i].up);
+                float dampForce = damperStiffness * springVelocity;
+
+                float springForce = springStiffness * springCompression;
+
+                float netForce = springForce - dampForce;
+
+                truckRB.AddForceAtPosition(netForce * rayPoints[i].up, rayPoints[i].position);
+
+                Debug.DrawLine(rayPoints[i].position, hit.point, Color.red);
+            }
+            else
+            {
+                wheelsIsGrounded[i] = 0;
+                Debug.DrawLine(rayPoints[i].position, rayPoints[i].position + (wheelRadius + maxLength) * -rayPoints[i].up, Color.red);
+            }
+        }
+    }
+
+    #endregion
 }
