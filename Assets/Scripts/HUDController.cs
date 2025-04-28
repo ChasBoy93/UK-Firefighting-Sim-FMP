0e7a8a9e811b20f973c37e20d5e0324b9c518edb
Charlie Wood <549750@chrome.escg.ac.uk>
2025-04-28 09:03:50 +0100

    Lots Added

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -5,9 +5,17 @@ public class HUDController : MonoBehaviour
 {
     public static HUDController instance;
 
+    //Controls
+    public GameObject controls;
+    bool controlsEnabled = false;
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        PlayerControlls();
     }
 
     [SerializeField] TMP_Text interactionText;
@@ -20,5 +28,14 @@ public class HUDController : MonoBehaviour
     public void DisabledInteractionText()
     {
         interactionText.gameObject.SetActive(false);
+    }
+
+    void PlayerControlls()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            controlsEnabled = !controlsEnabled; 
+            controls.SetActive(controlsEnabled);
+        }
     }
 }
---
+++ Assets/Scripts/TurnOffInstruction.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffInstruction : MonoBehaviour
+{
+    public GameObject buildingFireOne;
+    public GameObject crashOne;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            buildingFireOne.SetActive(false);
+        }
+    }
+}
